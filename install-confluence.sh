428f3e9d4b5a43fcf47c35ec7648d029eef5a371
Saeed Joulaei <sjoulaei@users.noreply.github.com>
2018-08-10 09:40:28 +1000

    updated to full unattended installation

--- install-confluence.sh
+++ install-confluence.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #Colours
 #RED="\033[31m"
 #GREEN="\033[32m"
@@ -7,41 +6,32 @@
 #RESET="\033[0m"
 
 #####################################################################
-# Get the inputs from user                                          #
+# Custom answers	                                            #
 #####################################################################
-read -p "Enter the confluence user name you want to create(confluence_user): " confluence_user
-confluence_user=${confluence_user:-confluence_user}
-read -sp "Enter the new confluence user password: " confluence_usr_pwd
-echo
-read -p "Enter the confluence database you want to create (confluence_db): " confluence_db
-confluence_db=${confluence_db:-confluence_db}
+#update the answers below as per your requirements
+# database name, user and password that Confluence will use
+confluence_user=confluence_user
+confluence_usr_pwd=ch@ngeTH!s
+confluence_db=confluence_db
 
 #copy your ssl certificates
-echo -e "For SSL certificates to work properly you need to copy the certificate files into the right location. I assume you have them in below addresses:"
-echo -e "  - certificate file: /etc/pki/tls/certs/your_cert_file.crt"
-echo -e "  - certificate key file: /etc/pki/tls/private/your_private_key_file.key"
-
-read -p "Enter the ssl certification file name (localhost.crt):" ssl_crt
-ssl_crt=${ssl_crt:-"localhost.crt"}
-read -p "Enter the ssl certification private key file name (localhost.key):" ssl_key
-ssl_key=${ssl_key:-"localhost.key"}
-
-read -p "Enter your server address (youraddress.com):" server_add
-server_add=${server_add:-"youraddress.com"}
-
-read -p "Enter your confluence HTTP Port Number (8090):" http_port
-http_port=${http_port:-"8090"}
-
-read -p "Enter your confluence Control Port Number (8000):" control_port
-control_port=${control_port:-"8000"}
-
-read -p "Enter the version of confluence you want to install(6.7.2):" confluence_ver
-confluence_ver=${confluence_ver:-"6.7.2"}
-
+#For SSL certificates to work properly you need to copy the certificate files into the right location. 
+#I assume you have created them and have them in below addresses:"
+#Default location: certificate file: /etc/pki/tls/certs/your_cert_file.crt"
+#Default location: certificate key file: /etc/pki/tls/private/your_private_key_file.key"
+ssl_crt="localhost.crt"
+ssl_key="localhost.key"
+server_add="youraddress.com"
+http_port="8090"
+control_port="8000"
+#Confluence server version that you want to install.
+confluence_ver="6.10.1"
 #Java keystore password default value. Default value most certainly hasn't been changed.
 keystore_pwd=changeit
-###################################################################ee
 
+###################################################################
+#Start Environment Preparation
+###################################################################
 #general prep
 echo -e "\033[32m Install some generic packages\033[0m"
 yum update -y
@@ -52,6 +42,12 @@ yum install -y  vim wget centos-release-scl\
 echo -e "\033[32mInstall packages you need for confluence\033[0m"
 yum install -y  postgresql96-server\
                 httpd24-httpd httpd24-mod_ssl httpd24-mod_proxy_html
+
+#Selinux config mode update to permissive
+
+echo -e "\033[32mFor apache to work properly with ssl, change the mode to permissive\033[0m"
+sed -i 's/^SELINUX=.*/SELINUX=permissive/' /etc/selinux/config && echo SUCCESS || echo FAILURE
+
 
 #setup database server
 /usr/pgsql-9.6/bin/postgresql96-setup initdb
@@ -64,17 +60,11 @@ sed -i "s|host    all             all             127.0.0.1/32.*|host    all    
 systemctl enable postgresql-9.6
 systemctl start postgresql-9.6
 
-#prepare database: create database, user and grant permissions to the user
+#prepare database: 
+#create database, user and grant permissions to the user
 printf "CREATE USER $confluence_user WITH PASSWORD '$confluence_usr_pwd';\nCREATE DATABASE $confluence_db WITH ENCODING='UNICODE' LC_COLLATE='C' LC_CTYPE='C' TEMPLATE=template0;\nGRANT ALL PRIVILEGES ON DATABASE $confluence_db TO $confluence_user;" > myconf/confluence-db.sql
 
 sudo -u postgres psql -f myconf/confluence-db.sql
-
-
-#Selinux config mode update to permissive
-
-echo -e "\033[32mFor apache to work properly with ssl, change the mode to permissive\033[0m"
-sed -i 's/^SELINUX=.*/SELINUX=permissive/' /etc/selinux/config && echo SUCCESS || echo FAILURE
-
 
 
 #create customised files
@@ -82,6 +72,10 @@ cp -v CONF/httpd/confluence.conf myconf/
 cp -v CONF/confluence/server.xml myconf/
 cp -v CONF/confluence/response.varfile myconf/
 
+
+#############################################################################
+#Setup Apache Server
+#############################################################################
 #update confluence.conf virtual host file
 
 mkdir -pv /opt/rh/httpd24/root/var/www/confluence/logs/
@@ -99,8 +93,10 @@ systemctl start httpd24-httpd
 cp -v myconf/confluence.conf /opt/rh/httpd24/root/etc/httpd/conf.d/
 
 
-
+#############################################################################
 #download and prepare confluence
+#############################################################################
+
 sed -i "s|8090|$http_port|g" myconf/response.varfile  && echo "http port updated on successfully" || echo "server port update on confluence.conf failed"
 sed -i "s|8000|$control_port|g" myconf/response.varfile  && echo "control port updated on successfully" || echo "server port update on confluence.conf failed"
 
@@ -116,8 +112,9 @@ cp -v myconf/server.xml /opt/atlassian/confluence/conf/server.xml
 echo -e "\033[32mSSL certification is going to be added to confluence java keystore\033[0m"
 /opt/atlassian/confluence/jre/bin/keytool -import -alias $server_add -keystore /opt/atlassian/confluence/jre/lib/security/cacerts -storepass $keystore_pwd -file /etc/pki/tls/certs/$ssl_crt
 
-
-#reboot
+###############################################################################
+#finally reboot the server
+###############################################################################
 echo -e "\033[32mGreat!!! confluence installation completed successfully."
 echo "Your system needs to be rebooted before you can continue to setup your system from GUI."
 echo "After restart you need to complete the setup from a web browser. Navigate to: https://$server_add"
